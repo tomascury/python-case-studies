72b38a80878526be6831d06e32e3d155c89acd70
tomascury <tomascury@gmail.com>
2022-08-15 22:22:15 +0100

    Class Inheritance

---
+++ basics/classes/inheritance/Chef.py
@@ -0,0 +1,10 @@
+class Chef:
+
+    def make_chicken(self):
+        print("The Chef makes a chicken")
+
+    def make_salad(self):
+        print("The Chef makes a salad")
+
+    def make_special_dish(self):
+        print("The Chef makes a bbq ribs")
---
+++ basics/classes/inheritance/app.py
@@ -0,0 +1,9 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+myChef = Chef()
+print(myChef.make_special_dish())
+
+myChineseChef = ChineseChef()
+print(myChineseChef.make_fried_rice())
+print(myChineseChef.make_special_dish())
