f209c91d60182b477d453a146f0b5ced0c9224a7
tomascury <tomascury@gmail.com>
2022-08-15 22:08:40 +0100

    Class functions

--- basics/classes/app.py
+++ basics/classes/app.py
@@ -5,6 +5,8 @@ student2 = Student("Pam", "Art", 4.3, False)
 
 print(student1.name)
 print(student1.major)
+print(student1.on_honor_roll())
 
 print(student2.gpa)
 print(student2.is_on_probation)
+print(student2.on_honor_roll())
